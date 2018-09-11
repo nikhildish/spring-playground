123545d53dc411396c8949b64530200d9fa9afa7
sharma.nikhil <sharma.nikhil@sling.com>
2018-09-10 23:56:33 -0600

    Spring IOC: Exercise:01-Spring Word Count

---
+++ src/main/java/com/example/demo/WordCounter.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public  class WordCounter {
+
+    public WordCounter() {
+    }
+
+    public Map<String,Integer> count (String input) {
+        Integer count = 0;
+        input = input.replaceAll("\\p{Punct}", "");
+
+        Map<String, Integer> countMap = new HashMap<String, Integer>();
+        String[] splitString = input.split("\\s+");
+
+        for (int i = 0; i < splitString.length; i++) {
+            Integer n = countMap.get(splitString[i]);
+            n = (n == null) ? 1 : ++n;
+            countMap.put(splitString[i], n);
+        }
+        return countMap;
+    }
+
+
+
+}
--- src/main/java/com/example/demo/WordCounterController.java
+++ src/main/java/com/example/demo/WordCounterController.java
@@ -3,7 +3,6 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,16 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Result {
+@RestController
+public  class WordCounterController {
+    @Autowired
+    WordCounter wordCounter;
 
-    Integer result;
-
-    public Integer getResult() {
-        return result;
+    @PostMapping("/words/count")
+    public @ResponseBody
+    ResponseEntity<?> postWordCount(@RequestBody String input){
+        Map<String, Integer> countMap = new HashMap<>();
+        countMap = wordCounter.count(input);
+        return new ResponseEntity<Object>(countMap, HttpStatus.OK);
     }
-
-    public void setResult(Integer result) {
-        this.result = result;
-    }
-
 }
+
